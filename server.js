205a9d3d3b428929136801d9d2654de1f5cd4357
purch <burcinguzel@engineer.com>
2016-10-17 06:56:57 +0000

    second

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var app = express();
 
 app.listen(process.env.PORT,process.env.IP);
+app.use(express.static(__dirname + '/public'));
 
 app.get("/",function(req,res){
   res.redirect("/w3.css");
